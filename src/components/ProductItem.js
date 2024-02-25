15119815505b337354eed7cc961c6a201ee2b775
SUJAY N GOWDA <1sujay1>
2024-02-26 00:40:48 +0530

    Deploy app to GitHub Pages

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,14 +1,11 @@
 // ProductItem.js
 import React from "react";
+import apple1 from "./../images/apple1.jpg";
 
 const ProductItem = ({ product }) => {
   return (
     <div className="card h-100 product-card">
-      <img
-        src="https://via.placeholder.com/300"
-        className="card-img-top"
-        alt="Product"
-      />
+      <img src={`${apple1}`} className="card-img-top" alt="Product" />
       <div className="card-body">
         <h5 className="card-title">{product.name}</h5>
         <p className="card-text">Price: ${product.price}</p>
